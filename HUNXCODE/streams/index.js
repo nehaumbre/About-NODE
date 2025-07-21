443c323197a727f5720d782e0e2b96664fd4da45
DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>
2025-07-21 22:13:50 +0530

    Stream
    Signed-off-by: DESKTOP-CFE2FCP\Viushali <nehaumbre28@gmail.com>

---
+++ HUNXCODE/streams/index.js
@@ -0,0 +1,8 @@
+import { createReadStream } from "fs";
+// const stream = createReadStream("./data.txt", { highWaterMark: 90000 });
+const stream = createReadStream("./data.txt", { encoding: "utf8" });
+
+stream.on("data", (data) => {
+  console.log(data);
+});
+// run by using node index.js in terminal
